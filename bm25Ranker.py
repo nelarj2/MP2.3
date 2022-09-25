bfe81bb34de5a23138badacb7ccfd74979a627af
nelarj2 <111804150+nelarj2@users.noreply.github.com>
2022-09-25 15:41:49 -0500

    changestorankerfunction

---
+++ bm25Ranker.py
@@ -0,0 +1,27 @@
+import metapy 
+import pytoml
+# Build the query object and initialize a ranker
+query = metapy.index.Document()
+ranker = metapy.index.OkapiBM25(k1=1.2,b=0.75,k3=500)
+# To do an IR evaluation, we need to use the queries file and relevance judgements.
+ev = metapy.index.IREval('config.toml')
+# Load the query_start from config.toml or default to zero if not found
+with open('config.toml', 'r') as fin:
+        cfg_d = pytoml.load(fin)
+query_cfg = cfg_d['query-runner']
+
+query_start = query_cfg.get('query-id-start', 0)
+# We will loop over the queries file and add each result to the IREval object ev.
+num_results = 10
+f = open("bm25.avg_p.txt", "a")
+resultsbm = []
+with open('cranfield-queries.txt') as query_file:
+    for query_num, line in enumerate(query_file):
+        query.content(line.strip())
+        results = ranker.score(idx, query, num_results)                            
+        avg_p = ev.avg_p(results, query_start + query_num, num_results)
+        f.write("%s\n" % avg_p)
+        resultsbm.append(float(avg_p))
+        print("Query {} average precision: {}".format(query_num + 1, avg_p))
+ev.map()
+print("Mean average precision: {}".format(ev.map()))
--- search_eval.py
+++ search_eval.py
@@ -9,6 +9,9 @@ class InL2Ranker(metapy.index.RankingFunction):
     """
     Create a new ranking function in Python that can be used in MeTA.
     """
+
+    ranker = metapy.index.DirichletPrior(9)
+
     def __init__(self, some_param=1.0):
         self.param = some_param
         # You *must* call the base class constructor here!
@@ -20,8 +23,12 @@ class InL2Ranker(metapy.index.RankingFunction):
         For fields available in the score_data sd object,
         @see https://meta-toolkit.org/doxygen/structmeta_1_1index_1_1score__data.html
         """
-        return (self.param + sd.doc_term_count) / (self.param * sd.doc_unique_terms + sd.doc_size)
+        tfn = np.log2(1+(sd.avg_dl/sd.doc_size))
+        s1 = tfn/(tfn+self.param)
+        s2 = np.log2((sd.num_docs+1)/(self.param+.5))
 
+        # return (self.param + sd.doc_term_count) / (self.param * sd.doc_unique_terms + sd.doc_size)
+        return s1*s2
 
 def load_ranker(cfg_file):
     """
@@ -29,7 +36,9 @@ def load_ranker(cfg_file):
     The parameter to this function, cfg_file, is the path to a
     configuration file used to load the index. You can ignore this for MP2.
     """
-    return metapy.index.JelinekMercer()
+    return InL2Ranker() 
+
+    # return metapy.index.JelinekMercer()
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -57,11 +66,40 @@ if __name__ == '__main__':
 
     query = metapy.index.Document()
     print('Running queries')
+    f = open("inl2.avg_p.txt", "a")
     with open(query_path) as query_file:
         for query_num, line in enumerate(query_file):
             query.content(line.strip())
             results = ranker.score(idx, query, top_k)
             avg_p = ev.avg_p(results, query_start + query_num, top_k)
+            f.write("%s\n" % float(avg_p))
             print("Query {} average precision: {}".format(query_num + 1, avg_p))
     print("Mean average precision: {}".format(ev.map()))
     print("Elapsed: {} seconds".format(round(time.time() - start_time, 4)))
+
+
+
+
+
+#reading in  bm25 -> code for bm25ranker in bm25ranker.py
+x = []
+with open('bm25.avg_p.txt', 'r') as f:
+    for line in f:
+        if line: #avoid blank lines
+            x.append(float(line.strip()))
+
+# #reading in inl2
+y= []
+with open('inl2.avg_p.txt', 'r') as f:
+    for line in f:
+        if line: #avoid blank lines
+            y.append(float(line.strip()))
+
+
+
+t, p = scipy.stats.ttest_rel(x, y)
+print(t)
+print(p)
+f = open("significance.txt", "a")
+f.write("%s\n" % p)
+f.close()
